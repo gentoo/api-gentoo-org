35d6b98dddc8231d04b6e03aa8c47e4d93308164
Eugene Bright <eugene@bright.gdn>
2018-08-06 20:04:39 +0300

    sort_repositories.py: Add GPL3+ notice

--- bin/sort_repositories.py
+++ bin/sort_repositories.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
-# python version >= 3.5
+"""
+This script is aimed to automate repositories.xml file formatting.
+<repo> nodes are sorted by name field value as a part of the process.
+python version >= 3.5 is required
+
+Copyright (C) 2018 Eugene Bright <eugene@bright.gdn>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import sys
 import argparse
